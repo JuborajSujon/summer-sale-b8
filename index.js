a9e6ec1eac3c0c2d0762a4f3eb4706336c7399d8
Juboraj Sujon <mdsujon72@gmail.com>
2024-02-16 20:11:37 +0600

    Added title container list item event function

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 // select card container
 const cards = document.querySelectorAll(".card");
 
+// list number for title container
+let titleCount = 1;
+
 for (let i = 0; i < cards.length; i++) {
+  // excess all card elements
   const card = cards[i];
 
   card.addEventListener("click", function () {
-    console.log("clicked");
+    // get the element
+    const title = card.querySelector("h3").innerText;
+    const price = card.querySelector("span").innerText;
+    const titleContainer = document.querySelector("#title-container");
+
+    // create a new element for title container
+    const p = document.createElement("p");
+    p.innerText = `${titleCount}. ${title}`;
+
+    // add to title container
+    titleContainer.appendChild(p);
+
+    // increment title count
+    titleCount++;
   });
 }
